cc646cc38bdd1300bfc2ee94f05387bff6e875c0
Anish Dhamodaran <anishd19@gmail.com>
2017-06-16 18:03:15 +0530

    makes the basic layout

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-sidebar">
+          <h2>Welcome to React</h2>
+        </div>
+        <div className="App-main">
+          <h2>Welcome to React</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
